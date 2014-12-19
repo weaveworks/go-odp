f4817f66e7509af62fb677b6e8a4f5a3c92c9a7d
David Wragg <david@wragg.org>
2014-12-19 17:38:42 +0000

    Foundations of a command line tool

--- ovs/main.go
+++ ovs/main.go
@@ -3,30 +3,89 @@ package main
 import (
 	"os"
 	"fmt"
+	"strings"
 	"github.com/dpw/go-openvswitch/openvswitch"
 )
 
+func die(f string, a ...interface{}) {
+	fmt.Fprintf(os.Stderr, f, a...)
+	os.Stderr.WriteString("\n")
+	os.Exit(1)
+}
+
+type commandDispatch interface {
+	run(args []string, pos int)
+}
+
+type command func (args []string)
+
+func (cmd command) run(args []string, pos int) {
+	cmd(args[pos:])
+}
+
+type commandMap map[string]commandDispatch
+
+func (cm commandMap) run(args []string, pos int) {
+	if pos >= len(args) {
+		die("Subcommand required by \"%s\".  Try \"%s help\"", strings.Join(args[:pos], " "), os.Args[0])
+	}
+
+	cd, ok := cm[args[pos]]
+
+	if !ok {
+		die("Unknown command \"%s\".  Try \"%s help\"", strings.Join(args[:pos + 1], " "), os.Args[0])
+	}
+
+	cd.run(args, pos + 1)
+}
+
+var commands = commandMap {
+	"datapath": commandMap {
+		"create": command(createDatapath),
+		"delete": command(deleteDatapath),
+	},
+}
+
 func main() {
+	commands.run(os.Args, 1)
+}
+
+
+func createDatapath(args []string) {
 	dpif, err := openvswitch.NewDpif()
 	if err != nil {
 		panic(err)
 	}
 
-	dp, err := dpif.LookupDatapath(os.Args[1])
-	//err = dpif.EnumerateDatapaths()
-	//_, err = dpif.CreateDatapath("foo")
+	for _, name := range(args) {
+		_, err = dpif.CreateDatapath(name)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	err = dpif.Close()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func deleteDatapath(args []string) {
+	dpif, err := openvswitch.NewDpif()
 	if err != nil {
 		panic(err)
 	}
 
-	if dp == nil {
-		fmt.Fprintf(os.Stderr, "no datapath named \"%s\"\n", os.Args[1])
-		return
-	}
+	for _, name := range(args) {
+		dp, err := dpif.LookupDatapath(name)
+		if err != nil {
+			panic(err)
+		}
 
-	err = dp.Delete()
-	if err != nil {
-		panic(err)
+		err = dp.Delete()
+		if err != nil {
+			panic(err)
+		}
 	}
 
 	err = dpif.Close()
